02801cc5425a28a3ece5f8957adfad93e3e6309b
nahid <ovi.chowdhury@hotmail.com>
2019-01-07 11:15:34 +0600

    Mongoose reconnect solved

--- rest-api/config/mongo.js
+++ rest-api/config/mongo.js
@@ -1,31 +1,37 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 
 // Build the connection string
 var dbURI = 'mongodb://localhost/QuranDB';
 
 // Create the database connection
-mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connect(dbURI, { useNewUrlParser: true, auto_reconnect: true });
 
 // CONNECTION EVENTS
 // When successfully connected
 mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to ' + dbURI);
+    console.log('Mongoose default connection open to ' + dbURI);
 });
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+    mongoose.disconnect();
 });
 
 // When the connection is disconnected
 mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
+    console.log('Mongoose default connection disconnected');
+    mongoose.connect(dbURI, { useNewUrlParser: true, auto_reconnect: true });
 });
 
+mongoose.connection.on('reconnected', function () {
+    console.log('Mongoose default connection reconnected')
+})
+
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
-    console.log('Mongoose default connection disconnected through app termination');
-    process.exit(0);
-  });
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
 });
--- rest-api/routes/users.js
+++ rest-api/routes/users.js
@@ -10,95 +10,95 @@ var userValidator = require('../middlewares/userValidator');
 var keyPath = path.join(__dirname, '../', '/config/key.json');
 
 /* GET users listing. */
-router.get('/', auth.authenticate, async function(req, res, next) {
-  userModel.find({}, function(err, result) {
-    if(err) {
-      res.status(500).json({message : err});
-    }
-    else{
-      res.status(200).json(result);
-    }
-  });
-  
+router.get('/', auth.authenticate, async function (req, res, next) {
+	userModel.find({}, function (err, result) {
+		if (err) {
+			res.status(500).json({ message: err });
+		}
+		else {
+			res.status(200).json(result);
+		}
+	});
+
 });
 
 /* Create user */
-router.post('/create',[auth.authenticate, userValidator.create], async function(req, res) {
-  try{
-    let userInfo = req.body;
-    let salt = await bcrypt.genSalt(10);
-    let hashPass = await bcrypt.hash(userInfo.password, salt);
-    let newUser = new userModel({
-      username : userInfo.username,
-      password : hashPass,
-      email : userInfo.email,
-      userType : userInfo.userType
-    });
-    let result = await newUser.save();
-    res.status(200).json({
-      username : result.username,
-      email : result.email,
-      userType : result.userType,
-      regDate : result.regDate
-    });
-  }
-  catch(ex){
-    res.status(500).json({message : ex});
-  }
+router.post('/create', [auth.authenticate, userValidator.create], async function (req, res) {
+	try {
+		let userInfo = req.body;
+		let salt = await bcrypt.genSalt(10);
+		let hashPass = await bcrypt.hash(userInfo.password, salt);
+		let newUser = new userModel({
+			username: userInfo.username,
+			password: hashPass,
+			email: userInfo.email,
+			userType: userInfo.userType
+		});
+		let result = await newUser.save();
+		res.status(200).json({
+			username: result.username,
+			email: result.email,
+			userType: result.userType,
+			regDate: result.regDate
+		});
+	}
+	catch (ex) {
+		res.status(500).json({ message: ex });
+	}
 
 });
 
 
 /** updating a user */
-router.put('/update/:username',[auth.authenticate, userValidator.update], async function(req, res) {
-  try{
-    let salt = await bcrypt.genSalt(10);
-    let hashPass = await bcrypt.hash(req.body.password, salt);
-    let result = await userModel.findOneAndUpdate(
-      {username : req.params.username},
-      {username : req.body.username, password : hashPass, email : req.body.email, userType : req.body.userType}
-    );
-    res.status(200).json(result);
-  }
-  catch(ex){
-    res.status(500).json({message : ex});
-  }
-    
+router.put('/update/:username', [auth.authenticate, userValidator.update], async function (req, res) {
+	try {
+		let salt = await bcrypt.genSalt(10);
+		let hashPass = await bcrypt.hash(req.body.password, salt);
+		let result = await userModel.findOneAndUpdate(
+			{ username: req.params.username },
+			{ username: req.body.username, password: hashPass, email: req.body.email, userType: req.body.userType }
+		);
+		res.status(200).json(result);
+	}
+	catch (ex) {
+		res.status(500).json({ message: ex });
+	}
+
 });
 
 /** delete user */
-router.delete('/delete/:username',[auth.authenticate, userValidator.delete], function(req, res) {
-  userModel.findOneAndRemove({username : req.params.username}, function(err, result) {
-    if(err) {
-      res.status(500).json({message : err});
-    }
-    else{
-      res.status(200).json(result);
-    }
-  });
+router.delete('/delete/:username', [auth.authenticate, userValidator.delete], function (req, res) {
+	userModel.findOneAndRemove({ username: req.params.username }, function (err, result) {
+		if (err) {
+			res.status(500).json({ message: err });
+		}
+		else {
+			res.status(200).json(result);
+		}
+	});
 });
 
 
-router.post('/login',[userValidator.login], async function(req, res) {
-  try{
+router.post('/login', [userValidator.login], async function (req, res) {
+	try {
 
-    let userData = await userModel.findOne({username : req.body.username});
-    if(!userData) return res.status(401).json({message : "Invalid credentials"});
-    let isPassValid = await bcrypt.compare(req.body.password, userData.password);
-    if(!isPassValid) return res.status(401).json({message : "Invalid credentials"});
-    fs.readFile(keyPath, (err, data) => {
-      let keyFileData = JSON.parse(data);
-      let jwtToken = jwt.sign(
-        {_id : userData._id, username : userData.username, userType : userData.userType},
-        keyFileData.jwtPrivateKey
-      );
-      res.header('x-auth-token', jwtToken).status(200).json({message : "Authenticated"});
-    });
-    
-  }
-  catch(ex){
-    res.status(500).json({message : ex});
-  }
+		let userData = await userModel.findOne({ username: req.body.username });
+		if (!userData) return res.status(401).json({ message: "Invalid credentials" });
+		let isPassValid = await bcrypt.compare(req.body.password, userData.password);
+		if (!isPassValid) return res.status(401).json({ message: "Invalid credentials" });
+		fs.readFile(keyPath, (err, data) => {
+			let keyFileData = JSON.parse(data);
+			let jwtToken = jwt.sign(
+				{ _id: userData._id, username: userData.username, userType: userData.userType },
+				keyFileData.jwtPrivateKey
+			);
+			res.header('x-auth-token', jwtToken).status(200).json({ message: "Authenticated" });
+		});
+
+	}
+	catch (ex) {
+		res.status(500).json({ message: ex });
+	}
 });
 
 
