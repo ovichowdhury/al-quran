a0bdafd822f4d470cad9d38ce47b8e48856f96a0
nahid <ovi.chowdhury@hotmail.com>
2019-01-14 14:17:08 +0600

    search by name is added in searchQuran route

--- rest-api/routes/searchQuran.js
+++ rest-api/routes/searchQuran.js
@@ -4,7 +4,7 @@ const auth = require('../middlewares/auth');
 const quranSearchService = require('../services/quranSearchService');
 
 
-router.get('/by-surahname-and-ayatnum/:surahName/:ayatNum', [auth.authenticate], async function(req, res, next) {
+router.get('/by-surahname-and-ayatnum/:surahName/:ayatNum', async function(req, res, next) {
     try {
         let result = await quranSearchService.bySurahNameAndAyatNum(req.params.surahName, req.params.ayatNum);
         res.status(200).json(result);
@@ -14,6 +14,16 @@ router.get('/by-surahname-and-ayatnum/:surahName/:ayatNum', [auth.authenticate],
     }
 });
 
+
+router.get('/by-surahname/:surahName', async function(req, res, next) {
+    try {
+        let result = await quranSearchService.bySurahName(req.params.surahName);
+        res.status(200).json(result);
+    }
+    catch(ex) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
 
 
 
--- rest-api/services/quranSearchService.js
+++ rest-api/services/quranSearchService.js
@@ -34,6 +34,35 @@ async function bySurahNameAndAyatNum(surahName, ayatNum) {
 }
 
 
+async function bySurahName(surahName) {
+    let query = [
+        {
+            $match : {title : bookTitle}
+        },
+        {
+            $addFields : {
+                "ayats" : {
+                    $filter : {
+                        input : "$contents",
+                        as : "ayat",
+                        cond : {$eq: ["$$ayat.surahName", {$toLower : surahName} ]}
+                    }
+                }
+            }
+        },
+        {
+            $project : {
+                _id : 0,
+                ayats : 1
+            }
+        }
+    ];
+
+    let result = await bookModel.aggregate(query);
+    return result[0];
+}
+
 module.exports = {
-    bySurahNameAndAyatNum: bySurahNameAndAyatNum
+    bySurahNameAndAyatNum: bySurahNameAndAyatNum,
+    bySurahName: bySurahName
 }
