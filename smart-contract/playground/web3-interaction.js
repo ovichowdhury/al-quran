4c62de981d15cb850a0d6ec1ecdacfb7720e140d
nahid <ovi.chowdhury@hotmail.com>
2019-01-30 10:41:19 +0600

    Experimental-3: web3js interaction code playground added

--- smart-contract/playground/web3-interaction.js
+++ smart-contract/playground/web3-interaction.js
@@ -1,5 +1,6 @@
 var W3 = require('web3');
 var abi = require('./abi.js');
+const Tx = require('ethereumjs-tx');
 
 //console.log(abi);
 
@@ -7,7 +8,9 @@ const web3 = new W3(new W3.providers.HttpProvider('https://rinkeby.infura.io/v3/
 
 web3.eth.defaultAccount = "0xeB6241e6c10eAdB465e6098AD4ad3D9625C6D832";
 
-const QuranContract = new web3.eth.Contract(abi, '0xc8d4AF58A7F21215687b56aFb92066295A72967b', {
+var contractAddress = '0xc8d4AF58A7F21215687b56aFb92066295A72967b';
+
+const QuranContract = new web3.eth.Contract(abi, contractAddress, {
     from: web3.eth.defaultAccount, // default from address
     gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
 });
@@ -33,34 +36,40 @@ async function gettingAccountBalance() {
 //gettingAccountBalance();
 
 async function invokeSmartContract() {
-    
+
     let ayatHash = await QuranContract.methods.getAyat(2).call();
     console.log(ayatHash);
 }
 
-async function unlockAccount(){
+async function unlockAccount() {
     web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "ovi123456789", 600)
         .then(console.log('Account unlocked!'));
 }
 
-async function addAyatInQuranContract() {
-    QuranContract.methods.addAyat(2, "this-is-second-hash").send({from: web3.eth.defaultAccount})
-        .on('transactionHash', (hash) => {
-            console.log(hash);
-        })
-        .on('receipt', (receipt) => {
-            console.log(receipt);
-        })
-        .on('confirmation', (confirmationNumber, receipt) => {
-            console.log(confirmationNumber);
-        })
-        .on('error', console.error);
+async function sendTx() {
+    var privateKey = Buffer.from("09261d406dd9c8c308dcd4b05445195762c01f3226d4df6132cb57e0a28a53c1", 'hex');
+    let nonce = await web3.eth.getTransactionCount(web3.eth.defaultAccount);
+    console.log(nonce);
+    var rawTransaction = {
+        "from": web3.eth.defaultAccount,
+        "gasPrice": web3.utils.toHex(20 * 1e9),
+        "gasLimit": web3.utils.toHex(210000),
+        "to": contractAddress,
+        "value": "0x0",
+        "data": QuranContract.methods.addAyat(2, "this-is-2-hash").encodeABI(),
+        "nonce": web3.utils.toHex(nonce)
+    };
+
+    var transaction = new Tx(rawTransaction);
+
+    transaction.sign(privateKey);
+
+    web3.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
+        .on('transactionHash',console.log);
+
+
 }
 
-//addAyatInQuranContract();
+//sendTx();
 
-
-
-
-web3.eth.personal.unlockAccount(web3.eth.defaultAccount, "ovi123456789", 600)
-        .then((d) => console.log(d));
+invokeSmartContract();
