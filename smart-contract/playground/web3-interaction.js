dd1e9cef9f6cc84d44ea9c232ac041e39df1558f
nahid <ovi.chowdhury@hotmail.com>
2019-01-30 11:07:58 +0600

    Experimental-4: web3-interaction updated

--- smart-contract/playground/web3-interaction.js
+++ smart-contract/playground/web3-interaction.js
@@ -10,6 +10,8 @@ web3.eth.defaultAccount = "0xeB6241e6c10eAdB465e6098AD4ad3D9625C6D832";
 
 var contractAddress = '0xc8d4AF58A7F21215687b56aFb92066295A72967b';
 
+var priKey = "09261d406dd9c8c308dcd4b05445195762c01f3226d4df6132cb57e0a28a53c1";
+
 const QuranContract = new web3.eth.Contract(abi, contractAddress, {
     from: web3.eth.defaultAccount, // default from address
     gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
@@ -47,7 +49,7 @@ async function unlockAccount() {
 }
 
 async function sendTx() {
-    var privateKey = Buffer.from("09261d406dd9c8c308dcd4b05445195762c01f3226d4df6132cb57e0a28a53c1", 'hex');
+    var privateKey = Buffer.from(priKey, 'hex');
     let nonce = await web3.eth.getTransactionCount(web3.eth.defaultAccount);
     console.log(nonce);
     var rawTransaction = {
@@ -70,6 +72,32 @@ async function sendTx() {
 
 }
 
+async function sendTxForDelete() {
+    var privateKey = Buffer.from("09261d406dd9c8c308dcd4b05445195762c01f3226d4df6132cb57e0a28a53c1", 'hex');
+    let nonce = await web3.eth.getTransactionCount(web3.eth.defaultAccount);
+    console.log(nonce);
+    var rawTransaction = {
+        "from": web3.eth.defaultAccount,
+        "gasPrice": web3.utils.toHex(20 * 1e9),
+        "gasLimit": web3.utils.toHex(210000),
+        "to": contractAddress,
+        "value": "0x0",
+        "data": QuranContract.methods.deleteAyat(2).encodeABI(),
+        "nonce": web3.utils.toHex(nonce)
+    };
+
+    var transaction = new Tx(rawTransaction);
+
+    transaction.sign(privateKey);
+
+    web3.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
+        .on('transactionHash',console.log);
+
+
+}
+
 //sendTx();
 
 invokeSmartContract();
+
+//sendTxForDelete();
