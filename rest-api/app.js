0fe5684fa811aa3d49be6260f2f3e11c0f9e24fd
nahid <ovi.chowdhury@hotmail.com>
2019-02-13 08:56:44 +0600

    Morgan logging in file added

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+var fs = require('fs');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -16,6 +17,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// logging requests
+var logPath = path.join(__dirname,'/log/http.log');
+var accessLogStream = fs.createWriteStream(logPath, {flags: 'a'});
+app.use(logger('common', {
+    stream: accessLogStream
+}));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
