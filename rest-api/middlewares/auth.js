a46eecb4ca8ffa6a3f2477f28c8a6ccf2478e043
nahid <ovi.chowdhury@hotmail.com>
2019-03-05 09:02:43 +0600

    security patch added in auth middleware

--- rest-api/middlewares/auth.js
+++ rest-api/middlewares/auth.js
@@ -2,6 +2,22 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 const keyPath = path.join(__dirname, '../', '/config/key.json');
+const userModel = require('../models/user');
+
+
+async function isTokenUserExists(username) {
+    try{
+        let result = await userModel.findOne({username: username});
+        if(result == null)
+            return false; 
+        else
+            return true;
+    }
+    catch(ex){
+        console.log(ex);
+        throw ex;
+    }
+}
 
 var authenticate = async function(req, res, next) {
     try{
@@ -9,14 +25,21 @@ var authenticate = async function(req, res, next) {
         const token = req.headers["x-auth-token"];
         if(!token) return res.status(401).json({message : "No token provided"});
 
-        fs.readFile(keyPath, (err, data) => {
+        fs.readFile(keyPath, async (err, data) => {
             try{
                 if(err) throw err;
                 let keyFileData = JSON.parse(data);
                 let decodedResult = jwt.verify(token, keyFileData.jwtPrivateKey);
                 console.log(decodedResult);
-                req.user = decodedResult;
-                next();
+                let isTokExists = await isTokenUserExists(decodedResult.username);
+                //console.log("tok exists: ", isTokExists);
+                if(isTokExists == true){
+                    req.user = decodedResult;
+                    next();
+                }
+                else{
+                    return res.status(500).json({message: "Token user deleted"});
+                }
             }
             catch(ex){
                 return res.status(400).json({message : "Invalid token"});
